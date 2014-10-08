96aa956eda12025e6f17a59660d59d8751c9ed1d
Barry Wark <barrywark@gmail.com>
2014-10-08 16:51:43 -0400

    Ah, we can get actual JSON from Aker

--- src/osiris/handler.clj
+++ src/osiris/handler.clj
@@ -5,9 +5,7 @@
             [schema.core :as s]
             [schema.coerce :as coerce]
             [osiris.updates :refer [process]]
-            [osiris.schema :refer [NewUpdate]]
-            [clojure.data.json :as json]
-            [ring.util.codec :refer [base64-decode]]))
+            [osiris.schema :refer [NewUpdate]]))
 
 ;; --- Routes --- ;;
 (defapi app
@@ -18,20 +16,14 @@
             (ok "Osiris!"))
 
           (POST* "/updates" []
-            :body [msg-body s/Str] ;; Base64-encoded
+            :body [update NewUpdate]
             :summary "Processes an update from Aker"
             :header-params [x-aws-sqsd-msgid :- s/Str
                             x-aws-sqsd-queue :- s/Str
                             x-aws-sqsd-first-received-at :- s/Str
                             x-aws-sqsd-receive-count :- s/Str]
 
-            (let [ __ (clojure.pprint/pprint msg-body)
-                   parsed (json/read-str (String. (base64-decode (.getBytes msg-body "utf-8"))) :key-fn #(keyword %)) ; Parse JSON from B64 encoded message body
-
-                   __ (clojure.pprint/pprint parsed)
-                   update ((coerce/coercer NewUpdate coerce/json-coercion-matcher) parsed) ; Coerce to a NewUpdate
-
-                   update-info (-> update
+            (let [ update-info (-> update
                                 (assoc :sqs-msgid x-aws-sqsd-msgid)
                                 (assoc :sqs-queue x-aws-sqsd-queue)
                                 (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
--- test/osiris/test/handler.clj
+++ test/osiris/test/handler.clj
@@ -1,26 +1,26 @@
 (ns osiris.test.handler
   (:require [midje.sweet :refer :all]
             [osiris.handler :as handler]
-            [ring.mock.request :refer [request header content-type body]]
-            [clojure.data.json :as json]
-            [ring.util.codec :refer [base64-encode]]))
+            [ring.mock.request :refer [request header content-type body]]))
 
 (facts "About Osiris"
-       (fact "HEAD => 200"
-             (let [response (handler/app (request :head "/"))]
-               (:status response) => 200))
-       (fact "POST /updates => 200"
-             (let [ msg (String. (base64-encode (.getBytes "{\"database\" : \"my-database\"}" "utf-8")))
+  (fact "HEAD / => 200"
+    (let [response (handler/app (request :head "/"))]
+      (:status response) => 200))
+  (fact "GET / => 200"
+    (let [response (handler/app (request :get "/"))]
+      (:status response) => 200))
 
-                    post (-> (request :post "/updates")
-                               (header "X-Aws-Sqsd-Msgid" "123")
-                               (header "X-Aws-Sqsd-Queue" "queue")
-                               (header "X-Aws-Sqsd-First-Received-At" "12-12-12")
-                               (header "X-Aws-Sqsd-Receive-Count" "1")
-                               (content-type "application/json")
-                               (body msg))
-                    ]
-               (:status (handler/app post)) => 200
-               (provided
-                 (osiris.updates/process anything) => ())))
+  (fact "POST /updates => 200"
+    (let [post (-> (request :post "/updates")
+                 (header "X-Aws-Sqsd-Msgid" "123")
+                 (header "X-Aws-Sqsd-Queue" "queue")
+                 (header "X-Aws-Sqsd-First-Received-At" "12-12-12")
+                 (header "X-Aws-Sqsd-Receive-Count" "1")
+                 (content-type "application/json")
+                 (body "{\"database\" : \"my-database\"}"))]
+
+      (:status (handler/app post)) => 200
+      (provided
+        (osiris.updates/process anything) => ())))
   )
