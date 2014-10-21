9d651077af0a48459be1763a8325e59b7acc63df
Barry Wark <barrywark@gmail.com>
2014-10-21 16:11:16 -0400

    Check env and system property

--- src/osiris/config.clj
+++ src/osiris/config.clj
@@ -1,17 +1,17 @@
 (ns osiris.config)
 
-(def COUCH_HOST (if-let [host (System/getenv "COUCH_HOST")]
+(def COUCH_HOST (if-let [host (or (System/getenv "COUCH_HOST") (System/getProperty "COUCH_HOST"))]
                   host
                   "http://localhost:5995"))
 
-(def COUCH_DATABASE (if-let [database (System/getenv "COUCH_DATABASE")]
+(def COUCH_DATABASE (if-let [database (or (System/getenv "COUCH_DATABASE") (System/getProperty "COUCH_DATABASE"))]
                       database
                       "underworld_dev"))
 
-(def COUCH_USER (if-let [user (System/getenv "COUCH_USER")]
+(def COUCH_USER (if-let [user (or (System/getenv "COUCH_USER") (System/getProperty "COUCH_USER"))]
                   user
                   "ovation-io-dev"))
 
-(def COUCH_PASSWORD (if-let [password (System/getenv "COUCH_PASSWORD")]
+(def COUCH_PASSWORD (if-let [password (or (System/getenv "COUCH_PASSWORD") (System/getProperty "COUCH_PASSWORD"))]
                       password
                       "boom!"))
