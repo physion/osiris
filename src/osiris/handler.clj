98ee3f0fec8d164d9c7d968e3fd66ad1b7cb91a6
Barry Wark <barrywark@gmail.com>
2014-10-21 13:57:13 -0400

    Adding middleware logger

--- src/osiris/handler.clj
+++ src/osiris/handler.clj
@@ -4,6 +4,7 @@
             [ring.util.http-response :refer [ok]]
             [schema.core :as s]
             [schema.coerce :as coerce]
+            [ring.middleware.logger :as logger]
             [osiris.updates :refer [process]]
             [osiris.schema :refer [NewUpdate]]
             [osiris.log :as log]))
@@ -12,24 +13,25 @@
 
 ;; --- Routes --- ;;
 (defapi app
-        (swaggered "osiris"
-          (HEAD* "/" []
-            (ok ""))
-          (GET* "/" []
-            (ok "Osiris!"))
+  (middlewares [logger/wrap-with-logger]
+    (swaggered "osiris"
+      (HEAD* "/" []
+        (ok ""))
+      (GET* "/" []
+        (ok "Osiris!"))
 
-          (POST* "/updates" []
-            :body [update NewUpdate]
-            :summary "Processes an update from Aker"
-            :header-params [x-aws-sqsd-msgid :- s/Str
-                            x-aws-sqsd-queue :- s/Str
-                            x-aws-sqsd-first-received-at :- s/Str
-                            x-aws-sqsd-receive-count :- s/Str]
+      (POST* "/updates" []
+        :body [update NewUpdate]
+        :summary "Processes an update from Aker"
+        :header-params [x-aws-sqsd-msgid :- s/Str
+                        x-aws-sqsd-queue :- s/Str
+                        x-aws-sqsd-first-received-at :- s/Str
+                        x-aws-sqsd-receive-count :- s/Str]
 
-            (let [ update-info (-> update
-                                (assoc :sqs-msgid x-aws-sqsd-msgid)
-                                (assoc :sqs-queue x-aws-sqsd-queue)
-                                (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
-                                (assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))]
+        (let [update-info (-> update
+                            (assoc :sqs-msgid x-aws-sqsd-msgid)
+                            (assoc :sqs-queue x-aws-sqsd-queue)
+                            (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
+                            (assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))]
 
-              (ok (process update-info))))))
+          (ok (process update-info)))))))
