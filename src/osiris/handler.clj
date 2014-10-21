d6c286fadde4fbe13377eb34bbdb893bbafb8456
Barry Wark <barrywark@gmail.com>
2014-10-21 16:30:52 -0400

    Don't wrap with ring-logger

--- src/osiris/handler.clj
+++ src/osiris/handler.clj
@@ -14,26 +14,26 @@
 
 ;; --- Routes --- ;;
 (defapi app
-  (middlewares [logger/wrap-with-logger]
-    (swaggered "osiris"
-      (HEAD* "/" []
-        (ok ""))
-      (GET* "/" []
-        (ok "Osiris!"))
+  ;(middlewares [logger/wrap-with-logger])
+  (swaggered "osiris"
+    (HEAD* "/" []
+      (ok ""))
+    (GET* "/" []
+      (ok "Osiris!"))
 
-      (POST* "/updates" []
-        :body [update NewUpdate]
-        :summary "Processes an update from Aker"
-        :header-params [x-aws-sqsd-msgid :- s/Str
-                        x-aws-sqsd-queue :- s/Str
-                        x-aws-sqsd-first-received-at :- s/Str
-                        x-aws-sqsd-receive-count :- s/Str]
+    (POST* "/updates" []
+      :body [update NewUpdate]
+      :summary "Processes an update from Aker"
+      :header-params [x-aws-sqsd-msgid :- s/Str
+                      x-aws-sqsd-queue :- s/Str
+                      x-aws-sqsd-first-received-at :- s/Str
+                      x-aws-sqsd-receive-count :- s/Str]
 
-        (logging/debug update)
-        (let [update-info (-> update
-                            (assoc :sqs-msgid x-aws-sqsd-msgid)
-                            (assoc :sqs-queue x-aws-sqsd-queue)
-                            (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
-                            (assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))]
+      (logging/debug update)
+      (let [update-info (-> update
+                          (assoc :sqs-msgid x-aws-sqsd-msgid)
+                          (assoc :sqs-queue x-aws-sqsd-queue)
+                          (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
+                          (assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))]
 
-          (ok (process update-info)))))))
+        (ok (process update-info))))))
