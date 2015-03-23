738da94f66040435f3642be722d35db5e1903969
Barry Wark <barrywark@gmail.com>
2015-03-23 13:30:52 -0400

    Working logging

--- src/osiris/logging.clj
+++ src/osiris/logging.clj
@@ -3,12 +3,14 @@
             [osiris.config :refer [LOGGING_HOST]]))
 
 (defn setup! []
-  (if-let [log-host LOGGING_HOST]
+  (if-let [host LOGGING_HOST]
     (log-config/set-logger!
       :level :debug
-      :out (org.apache.log4j.net.SyslogAppender.
-             (org.apache.log4j.PatternLayout. "%p: (%F:%L) %x %m %n")
-             log-host
-             org.apache.log4j.net.SyslogAppender/LOG_LOCAL7))
+      :out (doto (org.apache.log4j.net.SyslogAppender.)
+             (.setSyslogHost host)
+             (.setFacility "LOCAL7")
+             (.setFacilityPrinting false)
+             (.setName "osiris"))) ;             (org.apache.log4j.PatternLayout. "%p: (%F:%L) %x %m %n")
+
     (log-config/set-logger!
       :level :debug)))
